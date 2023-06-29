07e0d8c9b5300a4bf673387629f2b207b0ddfd8b
unknown <guifalar@gmail.com>
2023-06-29 06:36:15 -0300

    segunda clase de js

---
+++ Clase 6 js/js/Ejercicio10.js
@@ -0,0 +1,8 @@
+
+        function suma (sumando_1, sumando_2) {
+        const total = sumando_1 + sumando_2;
+        return total;
+        }
+    
+
+     
---
+++ Clase 6 js/js/ejercicio4.js
@@ -0,0 +1,31 @@
+"use strict"
+
+/***** CICLOS *****/
+// 
+// Ciclos | Loops | Iteradores
+// 
+// 2 tipos: Definidos e indefinidos
+//
+// Definidos: Ciclo FOR
+//  
+// Indefinidos: Ciclo WHILE y Ciclo DO...WHILE
+//
+/******************/
+
+
+//********************************
+//*** Ciclo FOR | Ciclo Definido
+// Repetición = iteración
+
+// COMPONENTES DE UN CICLO FOR
+// Contador
+// Condición o evaluación
+// Paso | incrementador o decrementador
+
+
+
+
+
+    for (let i = 0; i < 22 ; i=i+2) {
+        document.write( i + ",");
+        }
